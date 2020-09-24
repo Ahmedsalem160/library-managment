b8f31380b4d658837c714b8f62cb6e1546a1305d
Mohamed Hany <53129060+mohamed9964@users.noreply.github.com>
2020-09-25 01:59:11 +0200

    Update register.blade.php

--- resources/views/students/register.blade.php
+++ resources/views/students/register.blade.php
@@ -60,5 +60,5 @@
 <!-- JS -->
 <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
 <script src="{{asset('js/main.js')}}"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 </html>
