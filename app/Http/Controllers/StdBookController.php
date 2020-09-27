a1e989d0a3a092cac3801955917d84cba732139d
Mohamed Hany Mohamed <m.hanyy996@gmail.com>
2020-09-27 18:47:27 +0200

    add

--- app/Http/Controllers/StdBookController.php
+++ app/Http/Controllers/StdBookController.php
@@ -49,7 +49,28 @@ class StdBookController extends Controller
         $book = book::find($id);
         return view("stdbooks.show", ['book' => $book]);
     }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
     public function showAll()
     {
         $books = Book::all();
